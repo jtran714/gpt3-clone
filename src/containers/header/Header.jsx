d8ec1021d7496a0acfc0d45b67006a9351601974
Jordan Tran <thai.jordan@gmail.com>
2023-03-07 17:24:15 -0800

    articles and blog component done

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
-          Let's build something amazing with GPT-3 OpenAI
+          Let's build something amazing with OpenAI
         </h1>
         <p>
           Lorem ipsum dolor sit amet. Cum corporis rerum et doloribus
